e02ef1b0aa3f5053e46cb78d26edf92e435fd206
L.P <parisot.laurent@gmail.com>
2017-10-19 12:34:36 +0200

    REST API service

--- src/main/java/com/lpa/springrestclientexamples/services/ApiServiceImpl.java
+++ src/main/java/com/lpa/springrestclientexamples/services/ApiServiceImpl.java
@@ -1,12 +1,23 @@
 package com.lpa.springrestclientexamples.services;
 
 import com.lpa.springrestclientexamples.api.domain.User;
+import com.lpa.springrestclientexamples.api.domain.UserData;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
+@Service
 public class ApiServiceImpl implements ApiService {
+    private RestTemplate restTemplate;
+
+    public ApiServiceImpl(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     @Override
     public List<User> getUsers(Integer limit) {
-        return null;
+        UserData userData = restTemplate.getForObject("http://apifaketory.com/api/user?limit=" + limit, UserData.class);
+        return userData.getData();
     }
 }
--- src/test/java/com/lpa/springrestclientexamples/services/ApiServiceImplTest.java
+++ src/test/java/com/lpa/springrestclientexamples/services/ApiServiceImplTest.java
@@ -3,12 +3,17 @@ package com.lpa.springrestclientexamples.services;
 import com.lpa.springrestclientexamples.api.domain.User;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class ApiServiceImplTest {
     @Autowired
     private ApiService apiService;
